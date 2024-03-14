2834540ecd8b34ca2adb089ac4ae45e6e355b080
praktyki <praktyki@sohbi.pl>
2024-03-14 11:08:25 +0100

    1.0.5.1 zmieniono stopke w wersji na telefon.

--- mobile.js
+++ mobile.js
@@ -1,5 +1,5 @@
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|OperaMini/i.test(navigator.userAgent) ) {
-        document.location = "wsp bielczyny 1.0.4.1 na telefon/index.html"
+        document.location = "wsp bielczyny 1.0.5 na telefon/index.html"
 }
 else{
         
