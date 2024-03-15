d2b2d73bcbe2009fecdd7dc6519c493c4d926610
praktyki <praktyki@sohbi.pl>
2024-03-15 11:42:38 +0100

    poprawiono program wykrywający czy user jest na mobile czy pc akt 1.0.6.1

---
+++ on pc/mobileindex.js
@@ -0,0 +1,6 @@
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|OperaMini/i.test(navigator.userAgent) ) {
+        document.location = "wsp bielczyny 1.0.5 na telefon/index.html"
+}
+else{
+        
+}
---
+++ wsp bielczyny 1.0.5 na telefon/on mb/pcgaleria.js
@@ -0,0 +1,6 @@
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|OperaMini/i.test(navigator.userAgent) ) {
+
+}
+else{
+        document.location = "../index.html" 
+}
